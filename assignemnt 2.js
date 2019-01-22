848f156b8604d1636ff41a0faac40f865e7521a0
ajay-banstola <ajay.banstola@gmail.com>
2019-01-22 17:10:05 +0545

    added assignemnt 2

---
+++ assignemnt 2.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const radius = 30;
+
+const circum = (radius) => { return 2*Math.PI*radius}
+const area = (radius) => {return Math.PI*radius*radius}
+//Ajay banstola
+console.log("The circumference of circle is " + circum(radius));
+console.log("The area of the cicle is " + area(radius));
