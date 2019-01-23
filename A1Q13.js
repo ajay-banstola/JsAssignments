6597d0a72dfdc2660c05406fc48be48b123e8ddc
ajay-banstola <ajay.banstola@gmail.com>
2019-01-24 04:57:10 +0545

    added question 13

---
+++ A1Q13.js
@@ -0,0 +1,33 @@
+"use strict";
+
+
+function magic(num){
+    let test =  num;
+    let sum = 0;
+    while (test >=1){
+        let test4 = test % 10;
+        console.log(test4);
+        sum = sum + test4;
+        test = Math.floor(test/10);
+        console.log(test);
+        console.log(sum);
+    }
+    let test2= sum;
+    let rev = 0;
+    while (test2 >=1){
+        let test3 = test2 % 10;
+        rev = rev*10+ test3;
+        test2 = Math.floor(test2 /10);
+        console.log(rev);
+    }
+    let prod = rev*sum;
+    if (num === prod){
+        console.log("It is magic number");
+    }
+    else{
+        console.log("not a magic number");
+    }
+}
+
+let num = 1729;
+magic(num);
